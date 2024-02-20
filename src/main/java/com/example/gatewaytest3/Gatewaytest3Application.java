b9283d623c8e13c0dc17b1d4e4a3b4cbfbd80dc7
Jelsiana Jennet <jelsianajennet@gmail.com>
2024-02-20 17:17:46 +0530

    Gateway

---
+++ src/main/java/com/example/gatewaytest3/Gatewaytest3Application.java
@@ -0,0 +1,13 @@
+package com.example.gatewaytest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gatewaytest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gatewaytest3Application.class, args);
+	}
+
+}
